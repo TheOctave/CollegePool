356a8ce0053ff6aa8b426feff3bbf5b4c6bef633
ibmeister <oyekanibro@yahoo.com>
2016-03-05 09:26:44 -0500

    CollegePool push

---
+++ js/controllers.js
@@ -0,0 +1,22 @@
+angular.module('app.controllers', [])
+  
+.controller('homeCtrl', function($scope) {
+
+})
+   
+.controller('availableRidesCtrl', function($scope) {
+
+})
+   
+.controller('cloudCtrl', function($scope) {
+
+})
+      
+.controller('signupCtrl', function($scope) {
+
+})
+   
+.controller('loginCtrl', function($scope) {
+
+})
+ 
---
+++ js/routes.js
@@ -0,0 +1,53 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('home', {
+    url: '/page1',
+    templateUrl: 'templates/home.html',
+    controller: 'homeCtrl'
+  })
+
+  .state('availableRides', {
+    url: '/page2',
+    templateUrl: 'templates/availableRides.html',
+    controller: 'availableRidesCtrl'
+  })
+
+  .state('cloud', {
+    url: '/page3',
+    templateUrl: 'templates/cloud.html',
+    controller: 'cloudCtrl'
+  })
+
+  .state('menu', {
+    url: '/side-menu21',
+    templateUrl: 'templates/menu.html',
+    abstract:true
+  })
+
+  .state('signup', {
+    url: '/page4',
+    templateUrl: 'templates/signup.html',
+    controller: 'signupCtrl'
+  })
+
+  .state('login', {
+    url: '/page5',
+    templateUrl: 'templates/login.html',
+    controller: 'loginCtrl'
+  })
+
+$urlRouterProvider.otherwise('/page1')
+
+  
+
+});
